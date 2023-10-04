047335c82cfd7ebdfcc9c549f53ff89e8a17166e
Somya <isomya13@gmail.com>
2023-10-04 21:04:53 +0530

    extracted data from token

---
+++ next-auth/src/app/api/users/me/route.tsx
@@ -0,0 +1,30 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import {connect} from "@dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request: NextRequest){
+    try {
+        const response = await getDataFromToken(request);
+        alert(response);
+
+        const user= User.findOne({
+            _id: response.id
+        })
+
+        return NextResponse.json({
+            message: "user found",
+            data: user
+        })
+    } catch (error: any) {
+        return NextResponse.json({
+            error:error.message
+        },
+        {status:400});
+    }
+}
+
+
--- next-auth/src/app/profile/page.tsx
+++ next-auth/src/app/profile/page.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 
 export default function ProfilePage (){
     const router = useRouter()
+    const [data, setData] = React.useState("");
     const Logout = async ()=>{
           try {
              const response = await axios.get("/api/users/logout")
@@ -17,16 +18,29 @@ export default function ProfilePage (){
             toast.error("Something went wrong")
           }
     }
+
+    const getDetails = async ()=>{
+        const res = await axios.get("/api/users/me");
+        setData(res.data.data._id)
+        console.log(res.data.data);
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 text-2xl">
    <h1>Profile</h1>
        <hr />
 
        <h1>Profile page</h1>
+       <h2>{data? "" : <Link
+       href={`/profile/${data}`}
+       >
+       </Link>
+       }</h2>
        <hr/>
        <button className="bg-red-500 px-3 py-2 text-white font-bold rounded my-3"
        onClick={Logout}
        >Logout</button>
+       <button className="bg-blue-500 px-3 py-2 text-white"
+       onClick={getDetails}>Get Details</button>
     </div>
     )
 }
