ab7b5abf33a81804bbc459d0e0f583486b48f3d3
Somya <isomya13@gmail.com>
2023-10-04 15:01:40 +0530

    logout done

--- next-auth/src/app/api/users/login/route.tsx
+++ next-auth/src/app/api/users/login/route.tsx
@@ -44,7 +44,6 @@ export async function POST(request: NextRequest){
         //create token
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: "1h"})
 
-
         const response = new NextResponse(
          JSON.stringify({message: "Login successfully", status: "200"})
         )
--- next-auth/src/app/profile/page.tsx
+++ next-auth/src/app/profile/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+import React from "react";
+import axios from "axios";
+import Link from "next/link"
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage (){
+    const router = useRouter()
+    const Logout = async ()=>{
+          try {
+             const response = await axios.get("/api/users/logout")
+             toast.success("Successfully logged out");
+             router.push('/signup')
+          } catch (error: any) {
+            console.log(error); 
+            toast.error("Something went wrong")
+          }
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 text-2xl">
    <h1>Profile</h1>
        <hr />
 
        <h1>Profile page</h1>
-        </div>
+       <hr/>
+       <button className="bg-red-500 px-3 py-2 text-white font-bold rounded my-3"
+       onClick={Logout}
+       >Logout</button>
+    </div>
     )
 }
